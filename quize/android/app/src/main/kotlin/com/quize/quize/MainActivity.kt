387396eb70b5bce087c69ad195c80e9749c512bb
mohammed_shuqair <eng.mohammed.shuqair@gmail.com>
2023-05-24 12:39:29 +0300

    change dir

--- quize/android/app/src/main/kotlin/com/quize/quize/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.quize.quize
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- quize/lib/model/user.dart
+++
@@ -1,6 +0,0 @@
-class User {
-  late String name;
-  late String id;
-  late String mail;
-  User({required this.name,required this.id,required this.mail});
-}
